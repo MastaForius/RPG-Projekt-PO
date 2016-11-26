6e8a574b520caa2454e49344de9c8a233fa4af92
Robert Maciuszonek <mastaforius@hotmail.com>
2016-11-26 16:32:48 +0100

    Menu
    stworzone główne menu

---
+++ Project PO - RPG/Menu.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace Project_PO___RPG
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Menu));
+            this.label1 = new System.Windows.Forms.Label();
+            this.Btn_NowaGra = new System.Windows.Forms.Button();
+            this.Btn_Kontynuuj = new System.Windows.Forms.Button();
+            this.Lbl_Tytul = new System.Windows.Forms.Label();
+            this.Btn_WczytajGre = new System.Windows.Forms.Button();
+            this.Btn_ZapiszGre = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(55, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(219, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Gra RPG - Projekt programowanie obiektowe";
+            // 
+            // Btn_NowaGra
+            // 
+            this.Btn_NowaGra.BackColor = System.Drawing.Color.Transparent;
+            this.Btn_NowaGra.BackgroundImage = global::Project_PO___RPG.Properties.Resources.decorative_gold_frame_on_a_black_gradient_background_przycisk;
+            this.Btn_NowaGra.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Btn_NowaGra.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Btn_NowaGra.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_NowaGra.Font = new System.Drawing.Font("Curlz MT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_NowaGra.Location = new System.Drawing.Point(409, 100);
+            this.Btn_NowaGra.Name = "Btn_NowaGra";
+            this.Btn_NowaGra.Size = new System.Drawing.Size(206, 75);
+            this.Btn_NowaGra.TabIndex = 1;
+            this.Btn_NowaGra.Text = "Nowa gra";
+            this.Btn_NowaGra.UseVisualStyleBackColor = false;
+            // 
+            // Btn_Kontynuuj
+            // 
+            this.Btn_Kontynuuj.BackColor = System.Drawing.Color.Transparent;
+            this.Btn_Kontynuuj.BackgroundImage = global::Project_PO___RPG.Properties.Resources.decorative_gold_frame_on_a_black_gradient_background_przycisk;
+            this.Btn_Kontynuuj.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Btn_Kontynuuj.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Btn_Kontynuuj.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_Kontynuuj.Font = new System.Drawing.Font("Curlz MT", 15.75F, System.Drawing.FontStyle.Bold);
+            this.Btn_Kontynuuj.Location = new System.Drawing.Point(409, 200);
+            this.Btn_Kontynuuj.Name = "Btn_Kontynuuj";
+            this.Btn_Kontynuuj.Size = new System.Drawing.Size(206, 75);
+            this.Btn_Kontynuuj.TabIndex = 2;
+            this.Btn_Kontynuuj.Text = "Kontynuuj";
+            this.Btn_Kontynuuj.UseVisualStyleBackColor = false;
+            // 
+            // Lbl_Tytul
+            // 
+            this.Lbl_Tytul.AutoSize = true;
+            this.Lbl_Tytul.BackColor = System.Drawing.Color.Transparent;
+            this.Lbl_Tytul.Font = new System.Drawing.Font("Curlz MT", 36F, System.Drawing.FontStyle.Bold);
+            this.Lbl_Tytul.ForeColor = System.Drawing.Color.Red;
+            this.Lbl_Tytul.Location = new System.Drawing.Point(340, 25);
+            this.Lbl_Tytul.Name = "Lbl_Tytul";
+            this.Lbl_Tytul.Size = new System.Drawing.Size(344, 63);
+            this.Lbl_Tytul.TabIndex = 4;
+            this.Lbl_Tytul.Text = "RPG Projekt PO\r\n";
+            // 
+            // Btn_WczytajGre
+            // 
+            this.Btn_WczytajGre.BackgroundImage = global::Project_PO___RPG.Properties.Resources.decorative_gold_frame_on_a_black_gradient_background_przycisk;
+            this.Btn_WczytajGre.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Btn_WczytajGre.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Btn_WczytajGre.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_WczytajGre.Font = new System.Drawing.Font("Curlz MT", 15.75F, System.Drawing.FontStyle.Bold);
+            this.Btn_WczytajGre.Location = new System.Drawing.Point(409, 400);
+            this.Btn_WczytajGre.Name = "Btn_WczytajGre";
+            this.Btn_WczytajGre.Size = new System.Drawing.Size(206, 75);
+            this.Btn_WczytajGre.TabIndex = 5;
+            this.Btn_WczytajGre.Text = "Wczytaj grę";
+            this.Btn_WczytajGre.UseVisualStyleBackColor = true;
+            // 
+            // Btn_ZapiszGre
+            // 
+            this.Btn_ZapiszGre.BackgroundImage = global::Project_PO___RPG.Properties.Resources.decorative_gold_frame_on_a_black_gradient_background_przycisk;
+            this.Btn_ZapiszGre.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Btn_ZapiszGre.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Btn_ZapiszGre.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_ZapiszGre.Font = new System.Drawing.Font("Curlz MT", 15.75F, System.Drawing.FontStyle.Bold);
+            this.Btn_ZapiszGre.Location = new System.Drawing.Point(409, 300);
+            this.Btn_ZapiszGre.Name = "Btn_ZapiszGre";
+            this.Btn_ZapiszGre.Size = new System.Drawing.Size(206, 75);
+            this.Btn_ZapiszGre.TabIndex = 6;
+            this.Btn_ZapiszGre.Text = "Zapisz grę";
+            this.Btn_ZapiszGre.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::Project_PO___RPG.Properties.Resources.decorative_gold_frame_on_a_black_gradient_background_przycisk;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Curlz MT", 15.75F, System.Drawing.FontStyle.Bold);
+            this.button1.Location = new System.Drawing.Point(409, 500);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(206, 75);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Wyjście";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Menu
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackgroundImage = global::Project_PO___RPG.Properties.Resources.Kraty;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1008, 681);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Btn_ZapiszGre);
+            this.Controls.Add(this.Btn_WczytajGre);
+            this.Controls.Add(this.Lbl_Tytul);
+            this.Controls.Add(this.Btn_Kontynuuj);
+            this.Controls.Add(this.Btn_NowaGra);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(1024, 720);
+            this.MinimumSize = new System.Drawing.Size(1024, 720);
+            this.Name = "Menu";
+            this.Text = "Menu Główne RPG";
+            this.Load += new System.EventHandler(this.Menu_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Btn_NowaGra;
+        private System.Windows.Forms.Button Btn_Kontynuuj;
+        private System.Windows.Forms.Label Lbl_Tytul;
+        private System.Windows.Forms.Button Btn_WczytajGre;
+        private System.Windows.Forms.Button Btn_ZapiszGre;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
